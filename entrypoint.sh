052454207dcfc170bd8b63441612edc223dde6e9
“ibrahim0814” <ibrahim.a.ali@vanderbilt.edu>
2019-08-07 11:41:02 -0700

    add debug echo statements

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,15 +4,19 @@ set -eu
 
 if [ $# -eq 0 ]
 then
-    bash <(curl -s https://codecov.io/bash)
+  echo "inside first if"
+  bash <(curl -s https://codecov.io/bash)
 elif [ "x$INPUT_TOKEN" == "x" ] && [ "x$INPUT_FILE" == "x" ]
 then
+  echo "inside both"
   bash <(curl -s https://codecov.io/bash) -t $INPUT_TOKEN -f $INPUT_FILE
 elif [ "x$INPUT_TOKEN" == "x" ]
 then
+  echo "inside token"
   bash <(curl -s https://codecov.io/bash) -t $INPUT_TOKEN
 elif [ "x$INPUT_FILE" == "x" ]
 then
+  echo "inside file"
   bash <(curl -s https://codecov.io/bash) -f $INPUT_FILE
 fi
 
