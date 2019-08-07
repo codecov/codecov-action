7cb6a4679da6dc5c6aab0dde6396d42b3781b5b0
“ibrahim0814” <ibrahim.a.ali@vanderbilt.edu>
2019-08-06 18:17:06 -0700

    exit with failing status

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set -eu
 if [ $# -eq 0 ]
   then
     echo "No arguments supplied. Please make sure to provide an upload token"
-    exit
+    exit 1
 fi
 
 bash <(curl -s https://codecov.io/bash) -t $1
