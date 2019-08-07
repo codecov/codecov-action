6905df41d5aaa0cd467dc9a5ee76d178224c2e8b
“ibrahim0814” <ibrahim.a.ali@vanderbilt.edu>
2019-08-07 11:44:00 -0700

    correct conditionals

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,15 +6,15 @@ if [ $# -eq 0 ]
 then
   echo "inside first if"
   bash <(curl -s https://codecov.io/bash)
-elif [ "x$INPUT_TOKEN" == "x" ] && [ "x$INPUT_FILE" == "x" ]
+elif [ "x$INPUT_TOKEN" != "x" ] && [ "x$INPUT_FILE" != "x" ]
 then
   echo "inside both"
   bash <(curl -s https://codecov.io/bash) -t $INPUT_TOKEN -f $INPUT_FILE
-elif [ "x$INPUT_TOKEN" == "x" ]
+elif [ "x$INPUT_TOKEN" != "x" ]
 then
   echo "inside token"
   bash <(curl -s https://codecov.io/bash) -t $INPUT_TOKEN
-elif [ "x$INPUT_FILE" == "x" ]
+elif [ "x$INPUT_FILE" != "x" ]
 then
   echo "inside file"
   bash <(curl -s https://codecov.io/bash) -f $INPUT_FILE
