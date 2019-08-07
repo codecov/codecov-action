0be151486eedcff10a2500f7c491fef17e2f301f
“ibrahim0814” <ibrahim.a.ali@vanderbilt.edu>
2019-08-07 11:39:29 -0700

    add file conditionals

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,19 +2,17 @@
 
 set -eu
 
-
-echo $INPUT_FILE
-
-if [ "x$INPUT_FILE" == "x"  ] || [ "x$INPUT_TOKEN" == "x"  ];                                                                
-then
-    echo "some variables are null"
-fi
-
-
 if [ $# -eq 0 ]
-  then
+then
     bash <(curl -s https://codecov.io/bash)
-else
-  bash <(curl -s https://codecov.io/bash) -t $1
+elif [ "x$INPUT_TOKEN" == "x" ] && [ "x$INPUT_FILE" == "x" ]
+then
+  bash <(curl -s https://codecov.io/bash) -t $INPUT_TOKEN -f $INPUT_FILE
+elif [ "x$INPUT_TOKEN" == "x" ]
+then
+  bash <(curl -s https://codecov.io/bash) -t $INPUT_TOKEN
+elif [ "x$INPUT_FILE" == "x" ]
+then
+  bash <(curl -s https://codecov.io/bash) -f $INPUT_FILE
 fi
 
