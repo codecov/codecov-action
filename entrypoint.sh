4b3f2da7282f19209a553f7093186c2361763208
“ibrahim0814” <ibrahim.a.ali@vanderbilt.edu>
2019-08-07 11:18:32 -0700

    use default args

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,18 +5,11 @@ set -eu
 
 echo $INPUT_FILE
 
-if [ "x$1" == "x"  ] || [ "x$2" == "x"  ];                                                                
+if [ "x$INPUT_FILE" == "x"  ] || [ "x$INPUT_TOKEN" == "x"  ];                                                                
 then
-    echo "both variables are null"
+    echo "some variables are null"
 fi
 
-if [ -z "$1" ] || [ -z "$2" ]; then
-  echo 'Need 2 parameter are not empty'
-fi
-
-
-echo $1
-echo $2
 
 if [ $# -eq 0 ]
   then
