e49c256d4a504faa07a3f537522f2a37feb27349
“ibrahim0814” <ibrahim.a.ali@vanderbilt.edu>
2019-08-07 18:05:04 -0700

    try using no args

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,47 +8,47 @@ then
   bash <(curl -s https://codecov.io/bash)
 elif [ "x$INPUT_TOKEN" != "x" ] && [ "x$INPUT_FILE" != "x" ] && [ "x$INPUT_FLAGS" != "x" ] && [ "x$INPUT_NAME" != "x" ]
 then
-  echo "inside all 4"
+  echo "inside all 4 DONE"
   bash <(curl -s https://codecov.io/bash) -t $INPUT_TOKEN -f $INPUT_FILE -F $INPUT_FLAGS -n $INPUT_NAME
 elif [ "x$INPUT_TOKEN" != "x" ] && [ "x$INPUT_FILE" != "x" ] && [ "x$INPUT_FLAGS" != "x" ]
 then
-  echo "inside token file flags"
+  echo "inside token file flags DONE"
   bash <(curl -s https://codecov.io/bash) -t $INPUT_TOKEN -f $INPUT_FILE -F $INPUT_FLAGS
 elif [ "x$INPUT_TOKEN" != "x" ] && [ "x$INPUT_FILE" != "x" ] && [ "x$INPUT_NAME" != "x" ]
 then
-  echo "inside token file name"
+  echo "inside token file name DONE"
   bash <(curl -s https://codecov.io/bash) -t $INPUT_TOKEN -f $INPUT_FILE -n $INPUT_NAME
 elif [ "x$INPUT_TOKEN" != "x" ] && [ "x$INPUT_NAME" != "x" ] && [ "x$INPUT_FLAGS" != "x" ]
 then
-  echo "inside token name flags"
+  echo "inside token name flags DONE"
   bash <(curl -s https://codecov.io/bash) -t $INPUT_TOKEN -n $INPUT_NAME -F $INPUT_FLAGS
 elif [ "x$INPUT_NAME" != "x" ] && [ "x$INPUT_FILE" != "x" ] && [ "x$INPUT_FLAGS" != "x" ]
 then
-  echo "inside name file flags"
+  echo "inside name file flags DONE"
   bash <(curl -s https://codecov.io/bash) -n $INPUT_NAME -f $INPUT_FILE -F $INPUT_FLAGS
 elif [ "x$INPUT_TOKEN" != "x" ] && [ "x$INPUT_FILE" != "x" ]
 then
-  echo "inside token file"
+  echo "inside token file DONE"
   bash <(curl -s https://codecov.io/bash) -t $INPUT_TOKEN -f $INPUT_FILE
 elif [ "x$INPUT_TOKEN" != "x" ] && [ "x$INPUT_FLAGS" != "x" ]
 then
-  echo "inside token flags"
+  echo "inside token flags DONE"
   bash <(curl -s https://codecov.io/bash) -t $INPUT_TOKEN -F $INPUT_FLAGS
 elif [ "x$INPUT_FLAGS" != "x" ] && [ "x$INPUT_FILE" != "x" ]
 then
-  echo "inside flags file"
+  echo "inside flags file DONE"
   bash <(curl -s https://codecov.io/bash) -F $INPUT_FLAGS -f $INPUT_FILE
 elif [ "x$INPUT_FLAGS" != "x" ] && [ "x$INPUT_NAME" != "x" ]
 then
-  echo "inside flags name"
+  echo "inside flags name DONE"
   bash <(curl -s https://codecov.io/bash) -F $INPUT_FLAGS -n $INPUT_NAME
 elif [ "x$INPUT_TOKEN" != "x" ] && [ "x$INPUT_NAME" != "x" ]
 then
-  echo "inside token name"
+  echo "inside token name DONE"
   bash <(curl -s https://codecov.io/bash) -t $INPUT_TOKEN -n $INPUT_NAME
 elif [ "x$INPUT_FILE" != "x" ] && [ "x$INPUT_NAME" != "x" ]
 then
-  echo "inside file name"
+  echo "inside file name DONE"
   bash <(curl -s https://codecov.io/bash) -f $INPUT_FILE -n $INPUT_NAME
 elif [ "x$INPUT_TOKEN" != "x" ]
 then
