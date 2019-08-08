bd3230c27503a6a426ae857887c76e10928e6fbd
“ibrahim0814” <ibrahim.a.ali@vanderbilt.edu>
2019-08-07 23:00:45 -0700

    correct logic for token requirement

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,8 @@ set -eu
 
 if [ $# -eq 0 ]
 then
-  bash <(curl -s https://codecov.io/bash)
+  echo "Please provide an upload token"
+  exit 1
 elif [ "x$INPUT_TOKEN" != "x" ] && [ "x$INPUT_FILE" != "x" ] && [ "x$INPUT_FLAGS" != "x" ] && [ "x$INPUT_NAME" != "x" ]
 then
   bash <(curl -s https://codecov.io/bash) -t $INPUT_TOKEN -f $INPUT_FILE -F $INPUT_FLAGS -n $INPUT_NAME
@@ -20,40 +21,19 @@ then
 elif [ "x$INPUT_TOKEN" != "x" ] && [ "x$INPUT_NAME" != "x" ] && [ "x$INPUT_FLAGS" != "x" ]
 then
   bash <(curl -s https://codecov.io/bash) -t $INPUT_TOKEN -n $INPUT_NAME -F $INPUT_FLAGS
-elif [ "x$INPUT_NAME" != "x" ] && [ "x$INPUT_FILE" != "x" ] && [ "x$INPUT_FLAGS" != "x" ]
-then
-  bash <(curl -s https://codecov.io/bash) -n $INPUT_NAME -f $INPUT_FILE -F $INPUT_FLAGS
 elif [ "x$INPUT_TOKEN" != "x" ] && [ "x$INPUT_FILE" != "x" ]
 then
   bash <(curl -s https://codecov.io/bash) -t $INPUT_TOKEN -f $INPUT_FILE
 elif [ "x$INPUT_TOKEN" != "x" ] && [ "x$INPUT_FLAGS" != "x" ]
 then
   bash <(curl -s https://codecov.io/bash) -t $INPUT_TOKEN -F $INPUT_FLAGS
-elif [ "x$INPUT_FLAGS" != "x" ] && [ "x$INPUT_FILE" != "x" ]
-then
-  bash <(curl -s https://codecov.io/bash) -F $INPUT_FLAGS -f $INPUT_FILE
-elif [ "x$INPUT_FLAGS" != "x" ] && [ "x$INPUT_NAME" != "x" ]
-then
-  bash <(curl -s https://codecov.io/bash) -F $INPUT_FLAGS -n $INPUT_NAME
 elif [ "x$INPUT_TOKEN" != "x" ] && [ "x$INPUT_NAME" != "x" ]
 then
   bash <(curl -s https://codecov.io/bash) -t $INPUT_TOKEN -n $INPUT_NAME
-elif [ "x$INPUT_FILE" != "x" ] && [ "x$INPUT_NAME" != "x" ]
-then
-  bash <(curl -s https://codecov.io/bash) -f $INPUT_FILE -n $INPUT_NAME
 elif [ "x$INPUT_TOKEN" != "x" ]
 then
   bash <(curl -s https://codecov.io/bash) -t $INPUT_TOKEN
-elif [ "x$INPUT_FILE" != "x" ]
-then
-  bash <(curl -s https://codecov.io/bash) -f $INPUT_FILE
-elif [ "x$INPUT_FLAGS" != "x" ]
-then
-  bash <(curl -s https://codecov.io/bash) -F $INPUT_FLAGS
-elif [ "x$INPUT_NAME" != "x" ]
-then
-  bash <(curl -s https://codecov.io/bash) -n $INPUT_NAME
 else
-  echo "Please provide valid arguments"
+  echo "Please provide an upload token and valid arguments"
   exit 1
 fi
