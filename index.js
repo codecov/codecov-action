5ba6e211a416e1ecd54a9745c04dc613807f0908
ibrahim0814 <ibrahim.a.ali@vanderbilt.edu>
2019-11-12 23:41:00 -0800

    ughhhhh

--- index.js
+++ index.js
@@ -3,34 +3,34 @@ const github = require('@actions/github');
 const request = require('request');
 const execSh = require('./node_modules/exec-sh/lib/exec-sh');
 
-const { exec } = require('child_process');
+//const { exec } = require('child_process');
 
 try {
   // `who-to-greet` input defined in action metadata file
   const name = core.getInput('name');
-  console.log(`Name: ${name}!`);
+  console.log(`Name: ${name}`);
   
   const token = core.getInput('token');
-  console.log(`Token: ${token}!`);
+  console.log(`Token: ${token}`);
 
   const flags = core.getInput('flags');
-  console.log(`Flags: ${flags}!`);
+  console.log(`Flags: ${flags}`);
 
   const file = core.getInput('file');
-  console.log(`File: ${file}!`);
+  console.log(`File: ${file}`);
 
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  //const payload = JSON.stringify(github.context.payload, undefined, 2)
+  //console.log(`The event payload: ${payload}`);
 
 
   request('http://codecov.io/bash',  (error, response, body) => {
-    console.log('error:', error); 
-    console.log('statusCode:', response && response.statusCode); 
+    //console.log('error:', error); 
+    //console.log('statusCode:', response && response.statusCode); 
     //console.log('body:', body); 
     
 
-    execSh([`export CODECOV_TOKEN=${token}`,body], true, (err, stdout, stderr)=>{
+    execSh([`export CODECOV_TOKEN=${token}`, body], true, (err, stdout, stderr)=>{
         //console.log("error: ", err);
         console.log("stdout: ", stdout);
         //console.log("stderr: ", stderr);
