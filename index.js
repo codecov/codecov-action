747c905e394a5fe11c51a9f6ea647903a5ced980
ibrahim0814 <ibrahim.a.ali@vanderbilt.edu>
2019-11-14 00:47:48 -0800

    more output

--- index.js
+++ index.js
@@ -51,9 +51,9 @@ try {
     //body = body.replace("set -e +o pipefail","")
 
     execFile(body,[], {shell: true, env:{CODECOV_TOKEN: 'e0f9f29c-c2e4-4dd3-b440-0c2bc6937859'}},(error, stdout, stderr) => {
-        //console.log("error: ", error);
+        console.log("error: ", error);
         console.log("stdout: ", stdout);
-        //console.log("stderr: ", stderr);
+        console.log("stderr: ", stderr);
     });  
 
 
