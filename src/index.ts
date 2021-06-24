cf7b8b83b2684cf789e9bbbd902c1ebf48f140db
Tom Hu <tomhu1096+gpg@gmail.com>
2021-06-24 10:50:37 -0400

    feat: Use new Codecov uploader

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,16 @@ const exec = require('@actions/exec');
 
 import buildExec from './buildExec';
 
-const {execArgs, options, failCi} = buildExec();
+const {failCi} = buildExec();
 
-exec.exec('bash', execArgs, options)
+exec.exec('bash', ['bash/linux', '-c'])
+    .catch((err) => {
+      core.setFailed(
+          `Codecov failed with the following error: ${err.message}`,
+      );
+    });
+
+exec.exec('codecov-linux')
     .catch((err) => {
       if (failCi) {
         core.setFailed(
