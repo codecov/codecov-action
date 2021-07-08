41b0e36bdf8f8942527e8e3f1169ba100c132343
Tom Hu <tomhu1096+gpg@gmail.com>
2021-07-08 13:00:12 -0700

    Add arguments

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,15 @@ const core = require('@actions/core');
 
 const uploader = require('../uploader/src/index');
 
-// import buildExec from './buildExec';
+import buildExec from './buildExec';
 
 // const {failCi} = buildExec();
 
 try {
-  // const {execArgs, options} = buildExec();
-  uploader.main();
+  const {execArgs, options} = buildExec();
+  uploader.main(execArgs, options);
 } catch (err) {
   core.setFailed(
       `Codecov: Encountered an unexpected error: ${err.message}`,
   );
 }
-// TODO - unlink file
