c5e0046a49583a19ea9234f4a3f5947ce30bf97b
Tom Hu <tomhu1096+gpg@gmail.com>
2021-07-08 17:09:08 -0700

    filename

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,13 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
 
-const uploader = require('../uploader/bin/codecov');
-
 import buildExec from './buildExec';
 
 // const {failCi} = buildExec();
 
 try {
   const {execArgs} = buildExec();
-  exec.exec('node', [uploader, execArgs])
+  exec.exec('node', ['../uploader/bin/codecov', execArgs])
       .catch((err) => {
         core.setFailed(
             `Codecov: Encountered an unexpected error: ${err.message}`,
