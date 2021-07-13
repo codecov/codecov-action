232beede24e5c15d3544856c00f8fbac7e097246
Tom Hu <tomhu1096+gpg@gmail.com>
2021-07-13 15:56:11 -0700

    Add execArgs and options

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@ const https = require('https');
 const core = require('@actions/core');
 const exec = require('@actions/exec');
 
-// import buildExec from './buildExec';
+import buildExec from './buildExec';
 
-// const {failCi} = buildExec();
+const {execArgs, options} = buildExec();
 
 try {
   const url = 'https://uploader.codecov.io/latest/codecov-linux';
@@ -21,7 +21,7 @@ try {
       // TODO - validate step
       fs.chmodSync(filename, '777');
 
-      exec.exec(filename).catch((err) => {
+      exec.exec(filename, execArgs, options).catch((err) => {
         core.setFailed(
             'Codecov: Failed to properly upload: ' +
             `${err.message}`,
