def81595a67c83cbb0a42ba9b624b97f4c7deb3d
Tom Hu <tom@codecov.io>
2023-05-15 16:45:34 -0400

    release: 3.1.4

--- dist/index.js
+++ dist/index.js
@@ -22072,7 +22072,7 @@ var core = __nccwpck_require__(2186);
 // EXTERNAL MODULE: ./node_modules/@actions/github/lib/github.js
 var github = __nccwpck_require__(5438);
 ;// CONCATENATED MODULE: ./package.json
-const package_namespaceObject = {"i8":"3.1.3"};
+const package_namespaceObject = {"i8":"3.1.4"};
 ;// CONCATENATED MODULE: ./src/buildExec.ts
 
 
