2921e97eb9c76e70b6f6cbaeea10b6d1e4f2f2d5
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com.>
2024-04-29 14:24:37 +0200

    style: Node Packages
    Add the `node:` prefix to imports of node internal packages, making it easier to differentiate

--- src/validate.ts
+++ src/validate.ts
@@ -1,7 +1,7 @@
-import * as crypto from 'crypto';
-import * as fs from 'fs';
+import * as crypto from 'node:crypto';
+import * as fs from 'node:fs';
+import * as path from 'node:path';
 import * as gpg from 'gpg';
-import * as path from 'path';
 
 import * as core from '@actions/core';
 import {request} from 'undici';
