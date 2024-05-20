52bd105ad4abce2e9283f01f0668389e0aeac271
Tom Hu <thomas.hu@sentry.io>
2024-05-20 18:35:10 +0700

    fix: add log statement

--- src/buildExec.ts
+++ src/buildExec.ts
@@ -30,6 +30,7 @@ const getGitService = (): string => {
 };
 
 const isPullRequestFromFork = (): boolean => {
+  core.info(`evenName: ${context.eventName}`);
   if (
     `${context.eventName}` !== 'pull_request' &&
     `${context.eventName}` !== 'pull_request_target'
