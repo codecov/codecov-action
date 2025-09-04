3f31c823f2e0f9e1716690f1c0a363ef78341677
Tom Hu <thomas.hu@sentry.io>
2025-09-04 14:25:12 +0200

    fix: update to use local app/ dir

---
+++ app/calculator.py
@@ -0,0 +1,15 @@
+class Calculator:
+
+    def add(x, y):
+        return x + y
+
+    def subtract(x, y):
+        return x - y
+
+    def multiply(x, y):
+        return x * y
+
+    def divide(x, y):
+        if y == 0:
+            return 'Cannot divide by 0'
+        return x * 1.0 / y
